78818678e6025720a09b73b9b9e6bb2800208db9
maxjdev <maxjramosdev@gmail.com>
2023-10-27 14:08:19 -0300

    Adding PessoaFisica como classe concreta

---
+++ mod-13/Tarefa_Interface-Heranca-Composicao-Agregacao/src/edu/java/maxjdev/PessoaFisica.java
@@ -0,0 +1,24 @@
+package edu.java.maxjdev;
+import edu.java.maxjdev.abstracts.Pessoa;
+/**
+ * @author maxdev
+ * Classe Concreta para pessoa fisica que extende da classe abstrata Pessoa, recebe CPF como atributo,
+ * Construtor, Getter (Setter removido por não ser utilizado no contexto do exercicio) e toString
+ */
+public class PessoaFisica extends Pessoa {
+//    Atributos
+    private final String cpf;
+//    Construtor
+    public PessoaFisica(String nome, String sobrenome, String cpf) {
+        super(nome, sobrenome);
+        this.cpf = cpf;
+    }
+//    Getters && Setteres(Setters removidos por não serem utilizados no contexto do exercicio)
+    public String getCpf() {
+        return cpf;
+    }
+//    ToString
+    public String toString() {
+        return "Cliente PF Nome: " + getNome() + " " + getSobrenome() + " - CPF: " + getCpf();
+    }
+}
