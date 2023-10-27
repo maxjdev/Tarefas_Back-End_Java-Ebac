70ae870758fc2348afe4168c3dc8913a07a3c10b
maxjdev <maxjramosdev@gmail.com>
2023-10-27 14:08:41 -0300

    Adding PessoaJuridica como classe concreta

---
+++ mod-13/Tarefa_Interface-Heranca-Composicao-Agregacao/src/edu/java/maxjdev/PessoaJuridica.java
@@ -0,0 +1,24 @@
+package edu.java.maxjdev;
+import edu.java.maxjdev.abstracts.Pessoa;
+/**
+ * @author maxdev
+ * Classe Concreta para pessoa juridica que extende da classe abstrata Pessoa, recebe CNPJ como atributo,
+ * Construtor, Getter (Setter removido por não ser utilizado no contexto do exercicio) e toString
+ */
+public class PessoaJuridica extends Pessoa {
+//    Atributos
+    private final String cnpj;
+//    Construtor
+    public PessoaJuridica(String nome, String sobrenome, String cnpj) {
+        super(nome, sobrenome);
+        this.cnpj = cnpj;
+    }
+//    Getters && Setters(Setters removidos por não serem utilizados no contexto do exercicio)
+    public String getCnpj() {
+        return cnpj;
+    }
+//    ToString
+    public String toString() {
+        return "Cliente PJ Nome: " + getNome() + " " + getSobrenome() + " - CNPJ: " + getCnpj();
+    }
+}
