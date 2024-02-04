8af6f0421d7c5c9a9a3f37d8749293e6784c8b5f
maxjdev <maxjramosdev@gmail.com>
2024-02-04 20:27:32 -0300

    add: test exercise using JUnit part 2

---
+++ mod-24/src/org/edu/java/dao/IContratoDao.java
@@ -0,0 +1,8 @@
+package org.edu.java.dao;
+
+public interface IContratoDao {
+    void salvar();
+    void excluir();
+    void buscar();
+    void atualizar();
+}
---
+++ mod-24/test/org/edu/java/ContratoServiceTest.java
@@ -0,0 +1,76 @@
+package org.edu.java;
+
+import org.edu.java.dao.ContratoDao;
+import org.edu.java.dao.IContratoDao;
+import org.edu.java.dao.mocks.ContratoDaoMock;
+import org.edu.java.service.ContratoService;
+import org.edu.java.service.IContratoService;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class ContratoServiceTest {
+
+    @Test
+    public void salvarTest() {
+        IContratoDao dao = new ContratoDaoMock();
+        IContratoService service = new ContratoService(dao);
+        String retorno = service.salvar();
+        assertEquals("Sucesso", retorno);
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void esperadoErroNoSalvarComBancoDeDadosTest() {
+        IContratoDao dao = new ContratoDao();
+        IContratoService service = new ContratoService(dao);
+        String retorno = service.salvar();
+        assertEquals("Sucesso", retorno);
+    }
+
+    @Test
+    public void excluirTest() {
+        IContratoDao dao = new ContratoDaoMock();
+        IContratoService service = new ContratoService(dao);
+        String retorno = service.excluir();
+        assertEquals("Sucesso", retorno);
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void esperadoErroNoExcluirComBancoDeDadosTest() {
+        IContratoDao dao = new ContratoDao();
+        IContratoService service = new ContratoService(dao);
+        String retorno = service.excluir();
+        assertEquals("Sucesso", retorno);
+    }
+
+    @Test
+    public void buscarTest() {
+        IContratoDao dao = new ContratoDaoMock();
+        IContratoService service = new ContratoService(dao);
+        String retorno = service.buscar();
+        assertEquals("Sucesso", retorno);
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void esperadoErroNoBuscarComBancoDeDadosTest() {
+        IContratoDao dao = new ContratoDao();
+        IContratoService service = new ContratoService(dao);
+        String retorno = service.buscar();
+        assertEquals("Sucesso", retorno);
+    }
+
+    @Test
+    public void atualizarTest() {
+        IContratoDao dao = new ContratoDaoMock();
+        IContratoService service = new ContratoService(dao);
+        String retorno = service.atualizar();
+        assertEquals("Sucesso", retorno);
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void esperadoErroNoAtualizarComBancoDeDadosTest() {
+        IContratoDao dao = new ContratoDao();
+        IContratoService service = new ContratoService(dao);
+        String retorno = service.atualizar();
+        assertEquals("Sucesso", retorno);
+    }
+}
